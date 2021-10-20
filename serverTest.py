9cf7b5ae1b1e21f2138284d4c4759cd9bfc89e00
Lucas Wolter <lucaswolter@comcast.net>
2021-10-20 16:29:47 -0700

    lab 3 part 1

---
+++ serverTest.py
@@ -0,0 +1,21 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = socket.gethostname()
+host_ip = socket.gethostbyname(host) # ip of device
+port = 8080
+s.bind((host_ip, port))
+
+s.listen(5)
+while True:
+	c, addr = s.accept()
+	from_client = ''
+	print('Got connection from ', addr)
+	while True:
+		data = c.recv(4096)
+		if not data: break
+		from_client = data
+		print(from_client.decod('ascii'))
+		c.send('I am SERVER'.encode())
+	c.close()
+	print('Client disconnected')
